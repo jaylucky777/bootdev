87182b5c8bc394e81a7efc974266cba37fdcaacc
jaylucky777 <31351769+jaylucky777@users.noreply.github.com>
2026-01-02 21:00:51 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 	m := http.NewServeMux()
-
 	m.HandleFunc("/", handlePage)
 
-	const port = "8010"
+	// Read port from environment variable, fallback to 8010
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8010"
+	}
+
 	srv := http.Server{
 		Handler:      m,
 		Addr:         ":" + port,
@@ -20,22 +25,7 @@ func main() {
 		ReadTimeout:  30 * time.Second,
 	}
 
-	// this blocks forever, until the server
-	// has an unrecoverable error
 	fmt.Println("server started on ", port)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
-}
-
-func handlePage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(200)
-	const page = `<html>
-<head></head>
-<body>
-	<p> Hello from Docker! I'm a Go server. </p>
-</body>
-</html>
-`
-	w.Write([]byte(page))
 }
